d2e6888d0015799a8a6a737b9cc7bc6a87673aa1
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2020-02-13 16:32:37 -0500

    makes port dynamic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 const startFrom = require('./server.js');
 //port
-const port = 5000;
+const port = process.env.PORT || 5000;
 //global middleware
 server.use(express.json());
 server.use(startFrom);
